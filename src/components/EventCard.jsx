8393ea11beb245514d1acbc6e98189f18326f23e
JEREMIE NERET <jeremie.neret@gmail.com>
2021-09-10 19:45:27 +0200

    search page and home ok

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,8 +3,6 @@ import parse from 'html-react-parser';
 import coeur_inactive from '../assets/coeur_inactive.png';
 import DisplayColors from './DisplayColors';
 
-
-
 export default function EventCard({ event }) {
 
     return (
@@ -20,20 +18,11 @@ export default function EventCard({ event }) {
                             <p className='tags'>#{event.tags}</p>
                             <img className='favori' src={coeur_inactive} alt='favori' />
                         </div>
-
-
-
-
                         <button
-                            onClick= {() => event.click}
-                            >
+                            onClick={() => event.click}
+                        >
                             <h1 className='title'>{event.title}</h1>
                         </button>
-
-
-
-
-                        
                         <p className='date'>{parse(event.date)}</p>
                         <p className="description">{parse(event.lead_text)}</p>
                     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
-import { Fragment, React } from 'react';
+import axios from 'axios';
+import { Fragment, React, useEffect, useState } from 'react';
+import { GET_LAST_EVENT } from '../tools/api';
+
+import EventCard from './EventCard';
 
 
 const Home = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                GET_LAST_EVENT)
+            setData(result.data.records)
+        }
+        fetchData();
+    }, [])
+
     return (
         <Fragment>
             <main className="home">
@@ -20,6 +35,27 @@ const Home = () => {
                     </div>
                 </div>
                 <h1 className='actu-title'>C'est tout frais :</h1>
+                <ul className='display-event-cards'>
+                    {data?.map((event, i) => {
+
+                        return (
+                            <li key={i}>
+                                <EventCard
+                                    event={{
+                                        i: i,
+                                        id: event.record.fields.id,
+                                        title: event.record.fields.title,
+                                        cover: event.record.fields.cover_url,
+                                        description: event.record.fields.description,
+                                        date: event.record.fields.date_description,
+                                        tags: event.record.fields.tags,
+                                        lead_text: event.record.fields.lead_text,
+                                    }}
+                                ></EventCard>
+                            </li>
+                        )
+                    })}
+                </ul>
             </main>
         </Fragment>
     )
